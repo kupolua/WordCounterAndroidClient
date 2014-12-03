06c43fe59576958413c2fc2e87ad0fea3ff64e6b
StKotok <stkotok@gmail.com>
2014-12-04 00:29:36 +0300

    uk localization

--- app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/EspressoTest.java
+++ app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/EspressoTest.java
@@ -1,16 +1,12 @@
 package com.qalight.javacourse.wordcounterandroidclient;
 
-
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.google.android.apps.common.testing.ui.espresso.Espresso;
-import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
-import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
-import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
-
-import org.hamcrest.Matchers;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 
 public class EspressoTest extends ActivityInstrumentationTestCase2<MainActivity> {
     @TargetApi(Build.VERSION_CODES.FROYO)
@@ -26,6 +22,6 @@ public class EspressoTest extends ActivityInstrumentationTestCase2<MainActivity>
 
     public void testEspressoClickingListViewPopulatesTextView() {
         //Given the ListView is populated
-        Espresso.onView(ViewMatchers.withId(R.id.buttonOk)).perform(ViewActions.click());
+        onView(withId(R.id.buttonOk)).perform(click());
     }
 }
