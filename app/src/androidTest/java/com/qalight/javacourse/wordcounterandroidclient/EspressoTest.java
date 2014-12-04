f0b79bf1242c29e919b92cb1cce96b8be41ca30c
iRipVanWinkle <indri@i.ua>
2014-12-04 14:18:32 -0800

    added espresso testing and language

--- app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/EspressoTest.java
+++ app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/EspressoTest.java
@@ -7,10 +7,7 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.google.android.apps.common.testing.ui.espresso.Espresso;
 import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
-import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
 import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
-
-import org.hamcrest.Matchers;
 
 public class EspressoTest extends ActivityInstrumentationTestCase2<MainActivity> {
     @TargetApi(Build.VERSION_CODES.FROYO)
