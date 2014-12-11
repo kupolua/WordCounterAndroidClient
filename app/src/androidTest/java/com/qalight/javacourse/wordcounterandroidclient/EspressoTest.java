00717844c2502dac86cd083865e53362d20c7e5c
iRipVanWinkle <indri@i.ua>
2014-12-10 23:38:19 -0800

    WORDS-476 Android: Testing: Enable cyrillic in Espresso

--- app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/EspressoTest.java
+++ app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/EspressoTest.java
@@ -3,24 +3,19 @@ package com.qalight.javacourse.wordcounterandroidclient;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.apps.common.testing.ui.espresso.Espresso;
+import com.google.android.apps.common.testing.ui.espresso.UiController;
+import com.google.android.apps.common.testing.ui.espresso.ViewAction;
 import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
 import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
 import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.hasSibling;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDescendantOfA;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.allOf;
 
 public class EspressoTest extends ActivityInstrumentationTestCase2<MainActivity> {
     @TargetApi(Build.VERSION_CODES.FROYO)
@@ -36,24 +31,51 @@ public class EspressoTest extends ActivityInstrumentationTestCase2<MainActivity>
 
     public void testEspressoClickingListViewPopulatesTextView() {
         //Given the ListView is populated
-        onView(withId(R.id.buttonOk)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.buttonOk)).perform(ViewActions.click());
     }
 
     public void testCountWords_header2() {
         String expectedText = "Получить статистику частоты употребления слов!";
 //        String expectedText = "Get most used words!";
 
-        onView(withId(R.id.aboutTitle)).check(matches(withText(expectedText)));
+        Espresso.onView(ViewMatchers.withId(R.id.aboutTitle)).check(ViewAssertions.matches(ViewMatchers.withText(expectedText)));
     }
 
     public void testCountWords_plainText() {
-        String inputText = "the two one TWO";
-        String expectedText = "the two one 2 1 1";
+        String inputText = "текст";
 
-        onView(withId(R.id.inputText)).perform(typeText(inputText));
-        onView(withId(R.id.buttonOk)).perform(click());
+        Espresso.onView(ViewMatchers.withId(R.id.inputText)).perform(new SetTextAction(inputText));
+        Espresso.onView(ViewMatchers.withId(R.id.buttonOk)).perform(ViewActions.click());
+
 //        onView(allOf(
 //                isDescendantOfA(result)));
 //        onView(withId(R.id.resultTable)).check(matches(withText(expectedText)));
+    }
+
+    private static class SetTextAction implements ViewAction {
+
+        private CharSequence mTextToSet;
+
+        public SetTextAction(CharSequence textToSet) {
+            mTextToSet = textToSet;
+        }
+
+        @Override
+        public Matcher<View> getConstraints() {
+            return Matchers.allOf(ViewMatchers.withEffectiveVisibility(
+                            ViewMatchers.Visibility.VISIBLE),
+                    ViewMatchers.isAssignableFrom(TextView.class)
+            );
+        }
+
+        @Override
+        public String getDescription() {
+            return "set text";
+        }
+
+        @Override
+        public void perform(UiController uiController, View view) {
+            ((TextView) view).setText(mTextToSet);
+        }
     }
 }
