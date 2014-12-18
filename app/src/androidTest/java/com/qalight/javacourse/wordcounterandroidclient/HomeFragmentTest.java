c8f091275d6c918f6ea6cc9ec870bcb03bd37402
Vladimir Kamenniy <vkamenniy@gmail.com>
2014-12-18 13:50:23 +0200

    WCAC-22 Android: Testing: get text results from table

--- app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/HomeFragmentTest.java
+++ app/src/androidTest/java/com/qalight/javacourse/wordcounterandroidclient/HomeFragmentTest.java
@@ -3,15 +3,25 @@ package com.qalight.javacourse.wordcounterandroidclient;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
 import junit.framework.Assert;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static com.qalight.javacourse.wordcounterandroidclient.utils.Util.getTableResult;
+
 public class HomeFragmentTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private Solo solo;
+    private int waitTime = 2000;
 
     @TargetApi(Build.VERSION_CODES.FROYO)
     public HomeFragmentTest() {
@@ -41,10 +51,15 @@ public class HomeFragmentTest extends ActivityInstrumentationTestCase2<MainActiv
         // when
         solo.enterText((EditText) solo.getView(R.id.inputText), inputText);
         solo.clickOnView(solo.getView(R.id.buttonOk));
+        Thread.sleep(waitTime);
+        Map<String, String> actualResult = getTableResult(solo);
 
         // then
-        String expectedText = "one";
-        Assert.assertTrue(solo.searchText(expectedText));
+        Map<String, String> expectedResult = new LinkedHashMap<String, String>() {{
+                put("one", "2");
+                put("two", "1");
+            }};
+        Assert.assertEquals(expectedResult, actualResult);
     }
 
     public void testStartExecute() throws Exception {
