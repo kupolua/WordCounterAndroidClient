37d88c6b0de7c78cf0cee74c238541afdc9b2a57
Vladimir Kamenniy <vkamenniy@gmail.com>
2015-01-12 20:18:51 +0200

    WCAC-20 Android: Add build to TeamCity

---
+++ app/src/androidTest/java/info/deepidea/wordcounter/androidclient/unitTest/RobolectricTest.java
@@ -0,0 +1,24 @@
+package info.deepidea.wordcounter.androidclient.unitTest;
+
+import android.app.Activity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import info.deepidea.wordcounter.androidclient.MainActivity;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class RobolectricTest {
+
+    @Test
+    public void testActivity() throws Exception {
+        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        Assert.assertTrue(activity != null);
+    }
+}
